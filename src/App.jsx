55f3d0ddd4e254e1c7b96e6a70a173a8da83cd0f
Memoryman2022 <rlacannon@gmail.com>
2024-03-06 18:00:43 +0100

    major changes, plaese review

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import "./App.css";
 
 import Navbar from "./components/Navbar.jsx";
-import Recipies from "./components/Recipe.jsx";
+import Recipes from "./components/Recipe.jsx";
 import Footer from "./components/Footer.jsx";
+import Home from "./page/Home.jsx";
+import About from "./page/About.jsx";
+import NotFound from "./page/NotFound.jsx";
+
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-	return (
-		<div className="App">
-			<Navbar />
-			<Recipies />
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="notfound" element={<NotFound />} />
+      </Routes>
+      <Recipes />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;

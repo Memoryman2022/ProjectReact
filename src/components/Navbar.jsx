283f2c54850cb26e1b8162cdc892df4929826490
Memoryman2022 <rlacannon@gmail.com>
2024-03-11 16:23:28 +0100

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import Logo from "../assets/Logo.png";
-import { Link } from "react-router-dom";
-export default function Navbar() {
+import { Link, useNavigate } from "react-router-dom";
+export default function Navbar({ recipeArray }) {
+  const navigate = useNavigate();
+
+  const handleRandomRecipe = () => {
+    if (recipes.length > 0) {
+      const randomIndex = Math.floor(MAth.random() * recipes.length);
+      const randomRecipe = recipes[randomindex];
+      navigate(`/recipes/${randomRecipe.id}`);
+    }
+  };
   return (
     <div className="navbar">
       <img className="logo" src={Logo} alt="image-logo" />
       <ul className="list">
         <Link to="/">HOME</Link>
         <Link to="/About">ABOUT</Link>
+        <button onClick={handleRandomRecipe}>RANDOM RECIPE</button>
       </ul>
     </div>
   );
